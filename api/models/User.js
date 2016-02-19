1079de98d024092552d0b30438205b4913ba5049
Nabil Echaouch <nabil.echaouch@avicennetechnology.com>
2016-02-19 17:12:41 +0100

    Initial commit

---
+++ api/models/User.js
@@ -0,0 +1,62 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+var bcrypt = require('bcrypt');
+
+module.exports = {
+
+  attributes: {
+    firstName: {
+      type: 'string'
+    },
+    lastName: {
+      type: 'string'
+    },
+    email: {
+      type: 'email'
+    },
+    provider: {
+      type: 'string'
+    },
+    provider_id: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    },
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
+    }
+  },
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          console.log(err);
+          cb(err);
+        } else {
+          user.password = hash;
+          cb(null, user);
+        }
+      });
+    });
+  },
+  beforeUpdate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          console.log(err);
+          cb(err);
+        } else {
+          user.password = hash;
+          cb(null, user);
+        }
+      });
+    });
+  }
+};
